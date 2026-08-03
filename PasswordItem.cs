50b26d11a51cdc9038f1e40ce7550761a91feedb
minyurovevgeniy <minyurovevgeniy>
2026-08-03 19:32:37 +0300

    Упрощение класса паролей.

--- PasswordItem.cs
+++ PasswordItem.cs
@@ -6,55 +6,13 @@ namespace PasswordManager
 {
     public class PasswordItem
     {
-        string _comment;
-        public string Comment
-        { 
-            get
-            {
-                return _comment;
-            }
-
-            set
-            {
-                _comment = value;
-
-            }
-        }
-
-        string _password = "";
-        public string Password
-        {
-            get
-            {
-                return _password;
-            }
-
-            set
-            {
-                _password = value;
-
-            }
-        }
+        string comment;
+        string password = "";
 
         public PasswordItem(string comment, string password)
         {
-            this._comment = comment;
-            this._password = password;
+            this.comment = comment;
+            this.password = password;
         }
-
-        /*
-        public void generatePassword(string chars,int passwordLength)
-        {
-            StringBuilder sb = new StringBuilder();
-            Random rand = new Random();
-
-            for (int i = 0; i < passwordLength; i++)
-            {
-                sb.Append(chars[rand.Next(chars.Length)]);
-            }
-
-            _password = sb.ToString();
-        }
-        */
     }
 }
