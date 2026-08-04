ce388d3c517bdccf504709d1d1f02a72bbf91e74
minyurovevgeniy <minyurovevgeniy>
2026-08-04 21:17:23 +0300

    Макет настроек

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -229,7 +229,6 @@ namespace PasswordManager
                     passwordsListView.Items.Add(passwordItem.password);
                 }
             }
-
-        }
+        }   
     }
 }
---
+++ SettingsWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PasswordManager
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public bool alphabetUpperCaseLetters = false;
+        public bool alphabetLowerCaseLetters = false;
+        public bool digits = false;
+        public bool specialChars = false;
+        public int passwordLength = 2;
+
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
